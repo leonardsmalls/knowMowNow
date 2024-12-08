126b6c3b332c09721aa63a07741d7b0fa150fead
leonardsmalls <github.banked144@passmail.net>
2024-12-07 22:20:42 -0600

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,41 @@ var webpackConfig = {
         publicPath: "/assets/"
     },
     mode: config.isProd ? "production" : "development",
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          // Add other loaders as needed
+        ],
+      },
+      resolve: {
+        extensions: ['.js', '.jsx'],
+        fallback: {
+          "crypto": require.resolve("crypto-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "https": require.resolve("https-browserify"),
+          "os": require.resolve("os-browserify/browser"),
+          "path": require.resolve("path-browserify"),
+          "querystring": require.resolve("querystring-es3"),
+          "url": require.resolve("url/"),
+          "buffer": require.resolve("buffer/"),
+          "assert": require.resolve("assert/"),
+          "timers": require.resolve("timers-browserify"),
+          "fs": false,
+          "child_process": false,
+          "net": false,
+          "tls": false,
+          "zlib": require.resolve("zlib-browserify"),
+          "string_decoder": require.resolve("string_decoder/"),
+          "http": require.resolve("stream-http"),
+          "vm": require.resolve("vm-browserify"),
+        },
+      }
 };
 
 module.exports = webpackConfig;
